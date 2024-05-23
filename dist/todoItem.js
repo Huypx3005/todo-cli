b7f54b74174dc7de4584fff1116365f842fd7782
Huypx3005 <Phamxuanhuy3005@gmail.com>
2024-05-23 23:31:05 +0700

    todo list in CLI

---
+++ dist/todoItem.js
@@ -0,0 +1,29 @@
+// export class TodoItem {
+//   public id: number;
+//   public task: string;
+//   public complete: boolean = false;
+//   public constructor(id: number, task: string, complete: boolean = false) {
+//     this.id = id;
+//     this.task = task;
+//     this.complete = complete;
+//   }
+//   public printDetails(): void {
+//     console.log(
+//       `${this.id}\t${this.task} ${this.complete ? "\t(complete)" : ""}`
+//     );
+//   }
+// }
+export class TodoItem {
+    id;
+    task;
+    complete;
+    constructor(id, task, complete = false) {
+        this.id = id;
+        this.task = task;
+        this.complete = complete;
+        // no statements required
+    }
+    printDetails() {
+        console.log(`${this.id}\t${this.task} ${this.complete ? "\t(complete)" : ""}`);
+    }
+}
